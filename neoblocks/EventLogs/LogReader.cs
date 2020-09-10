49e27f0e7ab28fec609852442dd08da03ccd324f
Thomas Lobker <thomas@mediaserve.nl>
2020-09-10 13:16:05 +0000

    Add NeoBlocks patches

---
+++ neoblocks/EventLogs/LogReader.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.AspNetCore.Http;
+using Neo.IO.Data.LevelDB;
+using Neo.IO.Json;
+using Neo.Ledger;
+using Neo.Network.RPC;
+using Neo.VM;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Snapshot = Neo.Persistence.Snapshot;
+
+namespace Neo.Plugins
+{
+    public class LogReader : Plugin, IRpcPlugin, IPersistencePlugin
+    {
+        private readonly DB db;
+
+        public override string Name => "EventLogs";
+
+        public LogReader()
+        {
+            db = DB.Open(Path.GetFullPath(Settings.Default.Path), new Options { CreateIfMissing = true });
+        }
+
+        public override void Configure()
+        {
+            Settings.Load(GetConfiguration());
+        }
+
+        public void PreProcess(HttpContext context, string method, JArray _params)
+        {
+        }
+
+        public JObject OnProcess(HttpContext context, string method, JArray _params)
+        {
+            if (method != "geteventlog") return null;
+            UInt256 hash = UInt256.Parse(_params[0].AsString());
+            if (!db.TryGet(ReadOptions.Default, hash.ToArray(), out Slice value))
+                throw new RpcException(-100, "Unknown transaction");
+            return JObject.Parse(value.ToString());
+        }
+
+        public void PostProcess(HttpContext context, string method, JArray _params, JObject result)
+        {
+        }
+
+        public void OnPersist(Snapshot snapshot, IReadOnlyList<Blockchain.ApplicationExecuted> applicationExecutedList)
+        {
+            WriteBatch writeBatch = new WriteBatch();
+
+            foreach (var appExec in applicationExecutedList)
+            {
+                JObject json = new JObject();
+                json["txid"] = appExec.Transaction.Hash.ToString();
+                json["executions"] = appExec.ExecutionResults.Select(p =>
+                {
+                    JObject execution = new JObject();
+                    execution["trigger"] = p.Trigger;
+                    execution["contract"] = p.ScriptHash.ToString();
+                    execution["vmstate"] = p.VMState;
+                    execution["gas_consumed"] = p.GasConsumed.ToString();
+
+                    try
+                    {
+                        execution["stack"] = p.Stack.Select(q => q.ToParameter().ToJson()).ToArray();
+                    }
+                    catch (InvalidOperationException)
+                    {
+                        execution["stack"] = "error: recursive reference";
+                    }
+                    execution["events"] = p.Events.Select(q =>
+                    {
+                        JObject Event = new JObject();
+                        Event["contract"] = q.ScriptHash.ToString();
+                        Event["type"] = q.Type.ToString();
+                        try
+                        {
+                            Event["state"] = q.State.ToParameter().ToJson();
+                        }
+                        catch (InvalidOperationException)
+                        {
+                            Event["state"] = "ERROR";
+                        }
+                        if (Event["type"].AsString() == "Contract.Create" || Event["type"].AsString() == "Contract.Migrate")
+                        {
+                            try
+                            {
+                                UInt160 script_hash = UInt160.Parse(q.ScriptHash.ToString());
+                                ContractState contract = snapshot.Contracts.TryGet(script_hash);
+                                Event["state"] = contract.ToJson();
+                            }
+                            catch (InvalidOperationException)
+                            {
+                                Event["state"] = "ERROR";
+                            }
+                        }
+                        return Event;
+                    }).ToArray();
+                    return execution;
+                }).ToArray();
+                writeBatch.Put(appExec.Transaction.Hash.ToArray(), json.ToString());
+            }
+            db.Write(WriteOptions.Default, writeBatch);
+        }
+
+        public void OnCommit(Snapshot snapshot)
+        {
+        }
+
+        public bool ShouldThrowExceptionFromCommit(Exception ex)
+        {
+            return false;
+        }
+    }
+}
